8791b0c23aaccadbc714a0054047b952061239a7
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-03 12:50:58 -0700

    Confirmed the button works on find/replace function

--- javascript.js
+++ javascript.js
@@ -1,14 +1,25 @@
 /**
- * changes the color of a selected paragraph
+ * changes the color of a selected paragraph when hovered over
  **/
-
 function mouseOver(){
 	document.getElementById("color").style.color = "red";
 }
 
+/**
+ * changes the color of a selected paragraph back when mose removed
+ **/
 function mouseOut(){
 	document.getElementById("color").style.color = "black";
 }
+
 /**
- *
+ * function using regular expressions to find a given string and replace it with another given string
  **/
+function findReplace(){
+	//var find = document.getElementById("find").value;
+	//var replace = document.getElementById("replace").value;
+	//var findExpression = new RegExp("[Tam]");
+	//var repText= findExpression.replace(replace);//use replace to find the text and replace
+	//document.getElementById("parareplace").value = repText;
+	document.getElementById("color").style.color = "red";
+}
