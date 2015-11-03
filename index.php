3ce02657ab33c8350cb3483a44381e2bfff67cab
Bradley Brown <tall.white.ninja@gmail.com>
2015-11-03 10:26:13 -0700

    created both files, and filled in the paragraphs

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<script type="text/javascript" src="javascript.js"></script>
+	</head>
+	<body>
+		<h1>Fun with Javascript!!!</h1>
+		<h2>Paragraph 1</h2>
+		<p>Capt. Malcolm Reynolds: [about protective goggles] Fine, I'll wear 'em. But I'll look like an idiot. Zo : I should think you'd be used to that, sir. Jayne Cobb: Shepherd Book once said to me, "If you can't do something smart, do something right." Capt. Malcolm Reynolds: We're going to need paint. We're going to need red paint. Jayne Cobb: [as Mal's talking to the Vault Guard] MAL! Capt. Malcolm Reynolds: Every heist he's gotta start yellin' my name! Dr. Simon Tam: [deciding to leave the ship] River, do you want to stay with them? River Tam: It isn't safe. Dr. Simon Tam: No. I fear it isn't safe any more. [walks away] River Tam: [quietly] For them.
+		</p>
+		<h2>Paragraph 2</h2>
+		<p>[seeing a whole fleet of Reaver ships coming at him]  The Operative: Target the Reavers. Target the Reavers! Target everyone! Somebody fire!
+			Fanty: Do you know that girl?  Capt. Malcolm Reynolds: I really don't.
+			Dr. Simon Tam: But I think when they triggered you, it somehow brought this up. This memory.  River Tam: It isn't mine. The memory, it isn't mine. And I shouldn't have to carry it. It isn't mine. Don't make me sleep again.  Dr. Simon Tam: I won't. I won't.  River Tam: Put a bullet to me. Bullet in the brain pan. Squish.  Dr. Simon Tam: Don't say that! Not ever. We'll get through this.  River Tam: Things are going to get much, much worse.
+			The Operative: Where are you hiding, little girl?
+			The Operative: Secrets are not my concern. Keeping them is.
+		</p>
+		<h2>Paragraph 3</h2>
+		<p>[after the hovercraft makes a crash landing in the hanger of Serenity. Simon runs to his sister]  Dr. Simon Tam: River?  River Tam: I swallowed a bug.
+			Capt. Malcolm Reynolds: So I'm noticing. Is there a problem we don't know of?  [pause]  Capt. Malcolm Reynolds: You got a sweet take kissing your foot. How about you take your 25% and we can talk about the next job?  Fanty: Well our end is forty, precious.  Jayne Cobb: My muscular buttocks it's forty.
+			Capt. Malcolm Reynolds: It should have been me that got hit.  Shepherd Book: The thought... had crossed my mind.
+			Zo : Do you really think any of us is gonna get through this?  [looks at the other crew members struggling with their guns]  Jayne Cobb: Well, I might.
+			The Operative: Where are you hiding, little girl?
+		</p>
+	</body>
+</html>
